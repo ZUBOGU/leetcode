f1de2697e08fd3939cf4e25bbfcaf2f6902be886
ZUBO GU <gu.zubo@berkeley.edu>
2017-08-23 20:23:12 -0700

    reverse linked list || with solution

--- LinkList/M. 92. Reverse Linked List II.java
+++ LinkList/M. 92. Reverse Linked List II.java
@@ -22,7 +22,38 @@ Given m, n satisfy the following condition:
 
 /*
 Solution 1: 
+Create a dummy node. move the pointer to position m, then reverse the next n-m+1 ListNode.
+Create the reverse LinkedList in place.
+In the end, Connect 3 piece of the linked list adn return dummy.next.
+Hanndler the corner case head == null and head.next == null
+since 1 <= n=m <= n, we can return head.
 
 Time complexity : O(n). 
 Space complexity : O(1). constant space.
 */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy, cur = head;
+        for (int i = 1; i < m; i++) {
+            pre = pre.next;
+            cur = cur.next;
+        }
+            
+        ListNode prev = null;
+        ListNode curr = cur;
+        for (int i = 0; i < n-m+1; i++) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        
+        pre.next = prev;
+        cur.next = curr;
+        return dummy.next;
+        
+    }
+}
