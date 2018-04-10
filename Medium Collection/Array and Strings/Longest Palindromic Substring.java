056d22ec483cbf79c56c8cbf53060c9e5fdb3586
ZUBOGU <gu.zubo@berkeley.edu>
2018-04-09 17:57:13 -0700

    medium string problems

--- Medium Collection/Array and Strings/Longest Palindromic Substring.java
+++ Medium Collection/Array and Strings/Longest Palindromic Substring.java
@@ -21,8 +21,30 @@ Output: "bb"
 
 */
 
+// Solve as a DP problem O(n^2) time and space
+// to solve space complexity, expand around center. there is 2n -1 center. Could be 1 or 2 chars as center.
 class Solution {
     public String longestPalindrome(String s) {
-        
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) 
+        {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) 
+            {
+            	start = i - (len - 1) / 2;
+            	end = i + len / 2;
+            }
+        }
+        return s.equals("") ? "" : s.substring(start, end + 1);
+    }
+
+    int expand(String s, int l, int r) {
+    	while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+    		l--;
+    		r++;
+    	}
+    	return r - l - 1;
     }
 }
