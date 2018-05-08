f0ad54b4ad71d225ff272d1ddc43e7e9c5aacd2c
ZUBOGU <gu.zubo@berkeley.edu>
2018-05-07 17:10:37 -0700

    finish Math problems

--- Medium Collection/Math/Fraction to Recurring Decimal.java
+++ Medium Collection/Math/Fraction to Recurring Decimal.java
@@ -21,57 +21,42 @@ Input: numerator = 2, denominator = 3
 Output: "0.(6)"
 */
 
-
 // First use divide two integers to get quotient and reminder
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-    	int[] res = divide(numerator, denominator);
-    	int quotient = res[0].toString();
-    	int reminder = res[1];
-    	 if (reminder == 0) {
-            return quotient.toString();
+        if (numerator == 0) {
+            return "0";
         }
-        
+        StringBuilder res = new StringBuilder();
+        // get the sign
+        res.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+
+        // Use long to avoid overflow.
+        long d1 = Math.abs((long) numerator);
+        long d2 = Math.abs((long) denominator);
+
+        // Integral part
+        res.append(d1 / d2);
+        long reminder = d1 % d2;
+        if (reminder == 0) return res.toString();
+
         // fractional part
-        quotient.append(".");
+        res.append(".");
         HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-        map.put(reminder, quotient.length());
-        while (reminder != 0) {
+        while( reminder != 0) {
+            map.put(reminder, res.length());
+
             reminder *= 10;
-            quotient.append(reminder / den);
-            reminder %= den;
-            if (map.containsKey(reminder)) {
-                int index = map.get(reminder);
-                quotient.insert(index, "(");
-                quotient.append(")");
+            res.append(reminder / d2);
+            reminder %= d2;
+
+            Integer index = map.get(reminder);
+            if (index != null) {
+                res.insert(index, "(");
+                res.append(")");
                 break;
             }
-            else {
-                map.put(reminder, quotient.length());
-            }
         }
-        return quotient.toString();
-    }
-
-    public int[] divide(int dividend, int divisor) {
-        if(dividend<<1 == 0 && divisor == -1) return new int[]{Integer.MAX_VALUE, 0};
-        boolean sign = (dividend > 0) ^ (divisor > 0);
-        long d1 = Math.abs((long) dividend);
-        long d2 = Math.abs((long) divisor);
-        return divideLong(d1, d2, sign);
-    }
-    public int divideLong(long dividend, long divisor, boolean sign) {       
-        long res = 0;
-        while (dividend >= divisor) {
-            long multiple = 1;
-            long temp_divisor = divisor;
-            while (dividend >= (temp_divisor << 1)) {
-                temp_divisor <<= 1;
-                multiple <<= 1;
-            }
-            dividend -= temp_divisor;
-            res += multiple;
-        }
-        return sign ? new int[]{(int) -res, (int) dividend} : new int[]{(int) res, (int) dividend} ; 
+        return res.toString();
     }
 }
