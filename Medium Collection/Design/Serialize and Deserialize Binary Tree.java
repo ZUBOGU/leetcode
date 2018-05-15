5d0e329a21f0e1d3214892c5aede26464d97f16c
ZUBOGU <gu.zubo@berkeley.edu>
2018-05-15 16:57:44 -0700

    serialize and deserialize binary tree

--- Medium Collection/Design/Serialize and Deserialize Binary Tree.java
+++ Medium Collection/Design/Serialize and Deserialize Binary Tree.java
@@ -28,15 +28,41 @@ Note: Do not use class member/global/static variables to store states. Your seri
  * }
  */
 public class Codec {
+    private static final String spliter = ",";
+    private static final String NN = "null";
+    private static int idx = 0;
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
     }
 
+    public void buildString(TreeNode root, StringBuilder sb) {
+        if (root == null)
+            sb.append(NN).append(spliter);
+        else {
+            sb.append(root.val).append(spliter);
+            buildString(root.left, sb);
+            buildString(root.right, sb);
+        }
+    }
+    
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        idx = 0;
+        String[] arr = data.split(spliter);
+        return buildTree(arr);
+    }
+
+    public TreeNode buildTree(String[] arr) {
+        String p = arr[idx++];
+        if (p.equals(NN)) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(p));
+        root.left = buildTree(arr);
+        root.right = buildTree(arr);
+        return root;
     }
 }
 
