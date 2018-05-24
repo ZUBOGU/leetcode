154a250c22b983542a02a3a7f149f1a23b5a8a25
ZUBOGU <gu.zubo@berkeley.edu>
2018-05-23 17:05:58 -0700

    Done for Design the Key

---
+++ Hash Table/Practical Application - Design the Key/Find Duplicate Subtrees.java
@@ -0,0 +1,91 @@
+/*Find Duplicate Subtrees
+
+Given a binary tree, return all duplicate subtrees. For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+Two trees are duplicate if they have the same structure with same node values.
+
+Example 1:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   2   4
+       /
+      4
+
+The following are two duplicate subtrees:
+
+      2
+     /
+    4
+
+and
+
+    4
+
+Therefore, you need to return above trees' root in the form of a list.  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//Approach #1: Depth-First Search [Accepted]Approach #1: Depth-First Search [Accepted]
+// O(n^2) time O(n^2) space 
+
+//  Algorithm
+// Perform a depth-first search, where the recursive function returns the serialization of the tree. 
+// At each node, record the result in a map, and analyze the map after to determine duplicate subtrees.
+class Solution {
+    Map<String, Integer> count;
+    List<TreeNode> ans;
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        count = new HashMap();
+        ans = new ArrayList();
+        collect(root);
+        return ans;
+    }
+
+    public String collect(TreeNode node) {
+        if (node == null) return "#";
+        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
+        count.put(serial, count.getOrDefault(serial, 0) + 1);
+        if (count.get(serial) == 2)
+            ans.add(node);
+        return serial;
+    }
+}
+
+// Approach #2: Unique Identifier [Accepted]
+ // O(n) time O(n) space 
+class Solution {
+    int t;
+    Map<String, Integer> trees;
+    Map<Integer, Integer> count;
+    List<TreeNode> ans;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        t = 1;
+        trees = new HashMap();
+        count = new HashMap();
+        ans = new ArrayList();
+        lookup(root);
+        return ans;
+    }
+
+    public int lookup(TreeNode node) {
+        if (node == null) return 0;
+        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
+        int uid = trees.computeIfAbsent(serial, x-> t++);
+        count.put(uid, count.getOrDefault(uid, 0) + 1);
+        if (count.get(uid) == 2)
+            ans.add(node);
+        return uid;
+    }
+}
