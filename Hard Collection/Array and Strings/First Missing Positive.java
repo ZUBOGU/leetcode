fcb13f705803f55524cb0efcac7c533188b8eea2
ZUBOGU <gu.zubo@berkeley.edu>
2018-05-29 17:21:36 -0700

    First Missing Positive

--- Hard Collection/Array and Strings/First Missing Positive.java
+++ Hard Collection/Array and Strings/First Missing Positive.java
@@ -22,8 +22,23 @@ Note:
 Your algorithm should run in O(n) time and uses constant extra space.
  */
 
-class Solution {
-    public int firstMissingPositive(int[] nums) {
-        
+ // Constant space and O(n)
+ class Solution {
+    public int firstMissingPositive(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
