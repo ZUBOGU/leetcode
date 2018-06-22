be63c704f41bcac0c486337c10ec0a12adcaba29
ZUBOGU <gu.zubo@berkeley.edu>
2018-06-22 10:48:14 -0700

    Done Wiggle Sort ||

--- Hard Collection/Sorting and Searching/Wiggle Sort II.java
+++ Hard Collection/Sorting and Searching/Wiggle Sort II.java
@@ -19,6 +19,56 @@ Follow Up:
 Can you do it in O(n) time and/or in-place with O(1) extra space?
  */
 
- // I can sort and re arrange the array which is O(n log n)
+   // I can sort and re arrange the array which is O(n log n)
+ // Better solution find Middle and do in-place swap.
+ // The newIndex method is the key. To find the right index to replace.
+ class Solution {
+    public void wiggleSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+			return;
+        } 
+        int n = nums.length;
 
- 
+        // Assume this is O(n) space and O(1) time.
+        // Step 1: Find the median  
+        int median = findKthLargest(nums, (n + 1) / 2);
+
+        // Step 2: partition within O(n)-time & O(1)-space. 
+        int left = 0, mid = 0, right = n - 1;
+        while (mid <= right) {
+            if (nums[newIndex(mid,n)] > median) {
+                swap(nums, newIndex(left++, n), newIndex(mid++,n));
+            }
+            else if (nums[newIndex(mid,n)] < median) {
+                swap(nums, newIndex(right--,n), newIndex(mid,n));
+            }
+            else {
+                mid++;
+            }
+        }
+    }
+
+    private int newIndex(int index, int n) {
+        // where (n | 1) calculates the nearest odd that is not less than n.
+        return (1 + 2*index) % (n | 1);
+    }
+
+    void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+      }
+
+	public int findKthLargest(int[] nums, int k) {
+
+	    final PriorityQueue<Integer> pq = new PriorityQueue<>();
+	    for(int val : nums) {
+	        pq.offer(val);
+
+	        if(pq.size() > k) {
+	            pq.poll();
+	        }
+	    }
+	    return pq.peek();
+	}
+}
