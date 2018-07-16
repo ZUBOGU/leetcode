a08d017f6c3dc2eee04ffac7b78bae37a108b791
ZUBOGU <gu.zubo@berkeley.edu>
2018-07-16 16:10:55 -0700

    Done Trapping Rain Water

--- Hard Collection/Others/Trapping Rain Water.java
+++ Hard Collection/Others/Trapping Rain Water.java
@@ -19,10 +19,25 @@ Output: 6 */
 // // O(n2) Time and O(1) space.
 // In brute force, we iterate over the left and right parts again and again just to find the highest bar size upto that index. 
 // But, this could be stored. Voila, dynamic programming.
-
-// Approach 3: Using stacks
 class Solution {
     public int trap(int[] height) {
-        
+        if (height == null || height.length == 0) return 0;
+        int ans = 0;
+        int size = height.length;
+        // 2 dp array.
+        int[] leftMax = new int[size];
+        int[] rightMax = new int[size];
+        leftMax[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            leftMax[i] = Math.max(leftMax[i-1],height[i]);
+        }
+        rightMax[size-1] = height[size-1];
+        for (int i = size-2; i >= 0 ; i--) {
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+        }
+        for (int i = 1; i < size-1; i++) {
+            ans += Math.max(0, Math.min(leftMax[i],rightMax[i]) - height[i]);
+        }
+        return ans;
     }
 }
